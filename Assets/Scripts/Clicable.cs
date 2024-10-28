b3bf5e63f2907e1dbda84cc96f1c4d407164f11b
Dardo4002 <134235183+Dardo4002@users.noreply.github.com>
2024-10-28 21:20:20 +0100

    Generar pipas + implementación sprites

--- Assets/Scripts/Clicable.cs
+++ Assets/Scripts/Clicable.cs
@@ -1,17 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Clicable : MonoBehaviour
 {
     [SerializeField] static public float puntos = 0;
     [SerializeField] static public int mejora_jugador = 0;
+    [SerializeField] public GameObject pipa;
     void Start()
     {
     }
     private void OnMouseDown()
     {
         puntos += mejora_jugador + 1;
+        CrearPipa();
+    }
+
+    void CrearPipa()
+    {
+        // Obtener la posición del clic en el mundo
+        Vector3 clickPos = Input.mousePosition;
+
+        // Convertir la posición del clic a coordenadas del mundo
+        clickPos.z = Camera.main.nearClipPlane; // Puedes ajustar esto según tu necesidad
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(clickPos);
+
+        // Calcular la posición local en el objeto
+        Vector3 localPosition = transform.InverseTransformPoint(worldPosition);
+
+        Debug.Log($"Clic en el objeto en posición local: {localPosition}");
+
+        Instantiate(pipa, localPosition, Quaternion.identity);
     }
 }
